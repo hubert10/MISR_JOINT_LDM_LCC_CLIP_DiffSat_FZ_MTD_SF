db36d4a36a4abb0d43840f6fa953e00594619a44
Hubert Kanyamahanga <kanyamahanga.hubert@aims-senegal.org>
2026-06-02 19:13:46 +0200

    first commit

--- data/dataset.py
+++ data/dataset.py
@@ -38,6 +38,7 @@ class FitDataset(Dataset):
         self.use_augmentation = use_augmentation
         self.use_metadata = config["use_metadata"]
         if self.use_metadata == True:
+            print("********************************************mormalized_mtd:", dict_files["MTD_AERIAL"].shape)
             self.list_metadata = np.array(dict_files["MTD_AERIAL"])
         self.ref_year = config["ref_year"]
         self.ref_date = config["ref_date"]
